2bbdc63712c784b4b6818cb6c1db86155c3473a4
labib08 <iklabib49@gmail.com>
2023-09-24 16:47:23 +1000

    Fixed the issue with the buttons and created a separate csv for the artist photo coordinates

--- src/Mewtify.java
+++ src/Mewtify.java
@@ -1,5 +1,7 @@
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
+import java.util.Scanner;
 
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
@@ -17,11 +19,16 @@ import bagel.MouseButtons;
 import bagel.Window;
 import bagel.util.Point;
 
-
-public class BagelTest extends AbstractGame {
+public class Mewtify extends AbstractGame {
     private final static int WINDOW_WIDTH = 1024;
     private final static int WINDOW_HEIGHT = 768;
     private final static String GAME_TITLE = "Mewtify";
+    private final static int MAX_SONGS = 6;
+    private static final Point SPOTIFY_TEXT_POINT = new Point(125, 85);
+    private static final String SPOTIFY_NAME = "Mewtify";
+    private static final Point PAUSE_BUTTON_POINT = new Point(510, 702);
+    private static final Point NEXT_BUTTON_POINT = new Point(600, 700);
+    private static final Point PREV_BUTTON_POINT = new Point(410, 700);
     private final Image BACKGROUND_IMAGE = new Image("res/background0.png");
     private final Image WHITE_LINE = new Image("res/double-white-road-lines.jpg");
     private final Image SPOTIFY_LOGO = new Image("res/BlueSpotify.png");
@@ -29,46 +36,37 @@ public class BagelTest extends AbstractGame {
     private final Image PAUSE_BUTTON = new Image("res/PauseButton.png");
     private final Image NEXT_BUTTON = new Image("res/NextButton.png");
     private final Font spotifyFont = new Font("res/conformable.otf", 150);
-    private static final Point SPOTIFY_TEXT_POINT = new Point(125, 85);
-    private static final String SPOTIFY_NAME = "Mewtify";
     private DrawOptions SIZE = new DrawOptions();
     private DrawOptions NEXT_ROTATION = new DrawOptions();
     private DrawOptions SPOTIFY_TEXT = new DrawOptions();
-    private DrawOptions ARTISTI_PHOTO_SIZE = new DrawOptions();
     private boolean isPlay = false;
     private boolean playOnce = false;
     private Clip clip;
     private Image ARTIST_PHOTO;
     private String[] songs = {"res/Tame Impala - Borderline (Single Version).wav", "res/BlindingLights.wav", "res/Shape_of_you.wav", "res/One_Dance.wav", "res/This-Is-What-You-Came-For.wav", "res/Stay.wav"};
     private String[] artistPhotos = {"res/BorderlinePic.png", "res/The_Weeknd_-_Blinding_Lights.png", "res/Shape_Of_You_(Official_Single_Cover)_by_Ed_Sheeran.png", "res/DrakeOneDance.png", "res/This_Is_What_You_Came_For_cover.png", "res/Stay.png"};
-    private int[] artistPhotosXCoordinates = {400, 400, 400, 600, 600, 600};
-    private int[] artistPhotosYCoordinates = {250, 400, 550, 250, 400, 550};
+    private String[] artistPhotosCover = {"res/BorderlineRe.png", "res/The_Weeknd_-_Blinding_Lights_Re.png", "res/Shape_Of_You_(Official_Single_Cover)_by_Ed_Sheeran_re.png", "res/DrakeOneDanceRe.png", "res/This_Is_What_You_Came_For_cover_Re.png", "res/StayRe.png"};
+    private Point[] artistPoints = new Point[MAX_SONGS];
+    private boolean flagCsv = true;
     private int currSong = 0;
     private int currArtist = 0;
+    private int currPoint = 0;
     private boolean flagSong = true;
     private int page = 1;
-
+    private double xCoordinate;
+    private double yCoordinate;
     int flag = 0;
 
-    public BagelTest(){
+    public Mewtify(){
         super(WINDOW_WIDTH, WINDOW_HEIGHT, GAME_TITLE);
-        /**
-        try {
-            File file = new File(songs[currSong]);
-            AudioInputStream audioStream = AudioSystem.getAudioInputStream(file);
-            clip = AudioSystem.getClip();
-            clip.open(audioStream);
-        } catch (UnsupportedAudioFileException | IOException | LineUnavailableException e) {
-            e.printStackTrace();
-        }
-        */
+
     }
 
     /**
      * The entry point for the program.
      */
     public static void main(String[] args) {
-        BagelTest game = new BagelTest();
+        Mewtify game = new Mewtify();
         game.run();
     }
 
@@ -95,6 +93,25 @@ public class BagelTest extends AbstractGame {
             clip.close();
         }
     }
+
+
+    private void readCSV() {
+        try (Scanner file = new Scanner(new FileReader("res/artistiPhotoPoint.csv"))) {
+            while (file.hasNextLine()) {
+                String[] pointData = file.nextLine().split(",");
+                xCoordinate = Double.parseDouble(pointData[0]);
+                yCoordinate = Double.parseDouble(pointData[1]);
+                artistPoints[currPoint] = new Point(xCoordinate, yCoordinate);
+                currPoint++;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        // A flag variable to make sure the program reads the csv file only once.
+        flagCsv = false;
+
+    }
+
     /**
      * Performs a state update. This simple example shows an image that can be controlled with the arrow keys, and
      * allows the game to exit when the escape key is pressed.
@@ -115,17 +132,20 @@ public class BagelTest extends AbstractGame {
         spotifyFont.drawString(SPOTIFY_NAME, SPOTIFY_TEXT_POINT.x, SPOTIFY_TEXT_POINT.y, SPOTIFY_TEXT);
 
         if (page == 1) {
-            ARTISTI_PHOTO_SIZE.setScale(.4,.4);
 
-            for (int i =0; i< artistPhotosXCoordinates.length; i++){
-                ARTIST_PHOTO = new Image(artistPhotos[i]);
-                ARTIST_PHOTO.draw(artistPhotosXCoordinates[i], artistPhotosYCoordinates[i], ARTISTI_PHOTO_SIZE);
+            if (flagCsv) {
+                readCSV();
             }
-            if (input.wasPressed(MouseButtons.LEFT)) {
-                System.out.println(ARTIST_PHOTO.getBoundingBox());
-                page = 2;
-                currSong = 5;
-                currArtist = 5;
+
+            for (int i =0; i< MAX_SONGS; i++){
+                ARTIST_PHOTO = new Image(artistPhotosCover[i]);
+                ARTIST_PHOTO.draw(artistPoints[i].x, artistPoints[i].y);
+                if ((input.wasPressed(MouseButtons.LEFT)) && (ARTIST_PHOTO.getBoundingBoxAt(artistPoints[i]).intersects(input.getMousePosition()))) {
+                    currSong = i;
+                    currArtist = i;
+                    page = 2;
+                    break;
+                }
             }
 
         }
@@ -138,17 +158,15 @@ public class BagelTest extends AbstractGame {
             ARTIST_PHOTO = new Image(artistPhotos[currArtist]);
             ARTIST_PHOTO.draw(500, 370);
 
-            NEXT_BUTTON.draw(600, 700);
+            NEXT_BUTTON.draw(NEXT_BUTTON_POINT.x, NEXT_BUTTON_POINT.y);
             NEXT_ROTATION.setRotation(Math.PI);
-            NEXT_BUTTON.draw(410, 700, NEXT_ROTATION);
+            NEXT_BUTTON.draw(PREV_BUTTON_POINT.x, PREV_BUTTON_POINT.y, NEXT_ROTATION);
 
             if (flagSong) {
                 loadSong(currSong);
             }
 
-            //RECTANGLE.drawRectangle(510, 500, 200, 50, colour.WHITE);
-
-            if ((input.wasPressed(MouseButtons.LEFT)) && (474 < input.getMouseX() && input.getMouseX() < 546 && 665 < input.getMouseY() && input.getMouseY() < 739) || (input.wasPressed(Keys.SPACE))){
+            if (input.wasPressed(MouseButtons.LEFT) && (PAUSE_BUTTON.getBoundingBoxAt(PAUSE_BUTTON_POINT).intersects(input.getMousePosition())) || (input.wasPressed(Keys.SPACE))) {
                 if (!isPlay) {
                 isPlay = true;
                 playOnce = true;
@@ -160,7 +178,7 @@ public class BagelTest extends AbstractGame {
                 }
             }
 
-            else if ((input.wasPressed(MouseButtons.LEFT)) && (562 < input.getMouseX() && input.getMouseX() < 638) && (666 < input.getMouseY() && input.getMouseY() < 734)) {
+            else if (input.wasPressed(MouseButtons.LEFT) && NEXT_BUTTON.getBoundingBoxAt(NEXT_BUTTON_POINT).intersects(input.getMousePosition())) {
                 playMusic(3);
                 if (currArtist == artistPhotos.length - 1) {
                     currArtist = 0;
@@ -174,7 +192,7 @@ public class BagelTest extends AbstractGame {
                 System.out.println("Forward");
             }
 
-            else if((input.wasPressed(MouseButtons.LEFT)) && (372 < input.getMouseX() && input.getMouseX() < 448) && (666 < input.getMouseY() && input.getMouseY() < 734)) {
+            else if (input.wasPressed(MouseButtons.LEFT) && NEXT_BUTTON.getBoundingBoxAt(PREV_BUTTON_POINT).intersects(input.getMousePosition())) {
                 playMusic(3);
                 if (currArtist == 0) {
                     currArtist = artistPhotos.length - 1;
@@ -189,7 +207,7 @@ public class BagelTest extends AbstractGame {
             }
 
             if (isPlay){
-                PAUSE_BUTTON.draw(510, 702);
+                PAUSE_BUTTON.draw(PAUSE_BUTTON_POINT.x, PAUSE_BUTTON_POINT.y);
                 flag = 1;
                 if (playOnce){
                     playMusic(flag);
@@ -197,8 +215,13 @@ public class BagelTest extends AbstractGame {
 
             }
             else {
-                PLAY_BUTTON.draw(510, 702);
+                PLAY_BUTTON.draw(PAUSE_BUTTON_POINT.x, PAUSE_BUTTON_POINT.y);
             }
+        }
+        if (input.wasPressed(Keys.Q)) {
+            playMusic(3);
+            page = 1;
+            flagSong = true;
         }
     }
 }
